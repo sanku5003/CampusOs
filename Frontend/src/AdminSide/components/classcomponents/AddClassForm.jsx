3268e89e199ddcea02c5f6c9a2fc02b1e3d7a988
sanku5003 <pariharsanket10@gmail.com>
2026-08-08 20:33:01 +0530

    some minor changes in ui

--- Frontend/src/AdminSide/components/classcomponents/AddClassForm.jsx
+++ Frontend/src/AdminSide/components/classcomponents/AddClassForm.jsx
@@ -20,35 +20,49 @@ const AddClassForm = (props) => {
       <hr className="w-[30%] ml-auto mr-auto mt-2 h-px bg-purple-500" />
 
       <form className="classForm">
-        <div>
+        <div className="classForm-input">
           <label htmlFor="class">Class</label>
           <div className="input-group">
             <i className="ri-artboard-line"></i>
-            <input type="text" placeholder="e.g. VI or 6"/>
+            <input type="text" placeholder="e.g. VI or 6" />
           </div>
         </div>
-        <div>
+        <div className="classForm-input">
           <label htmlFor="section">Section</label>
           <div className="input-group">
             <i className="ri-pages-line"></i>
-            <input type="text" placeholder="e.g. A , B "/>
+            <input type="text" placeholder="e.g. A , B " />
           </div>
         </div>
-        <div>
+        <div className="classForm-input">
           <label htmlFor="Room_no">Room No.</label>
           <div className="input-group">
-           <i className="ri-map-pin-4-line"></i>
-            <input type="text" placeholder="e.g. D301 "/>
+            <i className="ri-map-pin-4-line"></i>
+            <input type="text" placeholder="e.g. D301 " />
           </div>
         </div>
-        <div>
-          <label htmlFor="medium">Room No.</label>
+        <div className="classForm-input relative">
+          <label htmlFor="medium">Medium</label>
+          <br />
           <i class="ri-english-input"></i>
-          <select>
-            <option value="English" selected>English</option>
-            <option value="Hindi" selected>Hindi</option>
-            <option value="Other" selected>Others..</option>
+          <select className="mt-3 w-1/2 border border-[#1c295c] outline-0 rounded py-1.5 px-7 ">
+            <option className="bg-[#0d132a] " value="English" selected>
+              English
+            </option>
+            <option className="bg-[#0d132a] " value="Hindi">
+              Hindi
+            </option>
+            <option className="bg-[#0d132a] " value="Other">
+              Others..
+            </option>
           </select>
+        </div>
+
+        <br />
+        <br />
+        <div className="form-btn">
+         
+          <button className="primary-btn"> Add Class</button>
         </div>
       </form>
     </div>
